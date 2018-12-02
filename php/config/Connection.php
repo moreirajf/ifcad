5af27e98fb59004bca9b73da8bb014cd3308f9f4
rsavenhago <rafael.savenhago@gmail.com>
2018-12-02 21:59:48 -0200

    correção das estruturas das pastas

---
+++ php/config/Connection.php
@@ -0,0 +1,15 @@
+<?php
+    class ConnectionFactory{
+        private const host="localhost";
+        private const user="admin";
+        private const pass="admin";
+        private const db="SINAU";
+        public static function getConnection(): PDO{
+            $connection = new PDO("mysql:dbname=$db;host=$host", $user, $pass);
+            return $connection;
+        }
+    }
+    
+    
+
+?>
