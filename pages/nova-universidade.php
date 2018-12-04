d31faf957be178512d8850acdd93dac3d41fe45a
josefmoreira <jose.moreira@alunos.ifsuldeminas.edu.br>
2018-12-04 10:57:49 -0200

    correção endereço

--- pages/nova-universidade.php
+++ pages/nova-universidade.php
@@ -202,12 +202,24 @@
                                             <input id="input-nome" type="text" name="nome" class="form-control">
                                     </div>
                                     <div class="form-group">
+                                            <label for="input-rua">Rua: </label>
+                                            <input id="input-rua" type="text" name="rua" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                            <label for="input-numero">Número: </label>
+                                            <input id="input-numero" type="text" name="numero" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                            <label for="input-bairro">Bairro: </label>
+                                            <input id="input-bairro" type="text" name="bairro" class="form-control">
+                                    </div>
+                                    <div class="form-group">
                                             <label for="input-cidade">Cidade: </label>
                                             <input id="input-cidade" type="text" name="cidade" class="form-control">
                                     </div>
                                     <div class="form-group">
-                                            <label for="input-endereco">Endereço: </label>
-                                            <input id="input-endereco" type="text" name="endereco" class="form-control">
+                                            <label for="input-estado">Estado: </label>
+                                            <input id="input-estado" type="text" name="estado" class="form-control">
                                     </div>
                                     <div class="form-group">
                                             <label for="input-cep">CEP: </label>
